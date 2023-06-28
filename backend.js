e2a368983319c7ecee5d16e113d6b017c7e90ee3
Revokeez <revokeezjn@gmail.com>
2023-06-27 22:11:25 -0400

    Added more style and functionality

--- backend.js
+++ backend.js
@@ -2,8 +2,7 @@ let isRight = [1, 2];
 let selectedOpcion = [];
 let howManyCorrect = 0;
 
-function Answers(question, selected)
-{
+function Answers(question, selected) {
   selectedOpcion[question] = selected.value;
   id = "q" + question;
 
@@ -14,13 +13,10 @@ function Answers(question, selected)
   labels[9].style.backgroundColor = "white";
   selected.parentNode.style.backgroundColor = "green";
 }
-function CorrectAnswers()
-{
+function CorrectAnswers() {
   howManyCorrect = 0;
-  for(i = 0; i < isRight.length; i++)
-  {
-    if(selectedOpcion[i] == isRight[i])
-    {
+  for (i = 0; i < isRight.length; i++) {
+    if (selectedOpcion[i] == isRight[i]) {
       howManyCorrect++;
     }
   }
