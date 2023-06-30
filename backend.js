0b6d1e826149c1033fac942e42182936b1e7b647
Revokeez <revokeezjn@gmail.com>
2023-06-29 21:42:53 -0400

    Added more style and more structure

--- backend.js
+++ backend.js
@@ -1,21 +1,26 @@
 let isRight = [1, 2, 3, 1, 1, 1, 4, 4, 3, 2];
+let arraysLength = [2, 4, 4, 4, 4, 4, 4, 4, 4, 4];
+let selectedArraysPosition = [];
 let selectedOpcion = [];
 let howManyCorrect = 0;
 
 function Answers(question, selected) {
   selectedOpcion[question] = selected.value;
-  id = "q" + question;
+  let id = "q" + question;
+  selectedArraysPosition = arraysLength[question];
 
-  labels = document.getElementById(id).childNodes;
-  labels[3].style.backgroundColor = "white";
-  labels[5].style.backgroundColor = "white";
-  labels[7].style.backgroundColor = "white";
-  labels[9].style.backgroundColor = "white";
+  let labels = document.getElementById(id).childNodes;
+  e = 1;
+  for (i = 0; i < selectedArraysPosition; i++) {
+    e = e + 2;
+    labels[e].style.backgroundColor = "white";
+  }
   selected.parentNode.style.backgroundColor = "green";
 }
+
 function CorrectAnswers() {
   howManyCorrect = 0;
-  for (i = 0; i < isRight.length; i++) {
+  for (let i = 0; i < isRight.length; i++) {
     if (selectedOpcion[i] == isRight[i]) {
       howManyCorrect++;
     }
